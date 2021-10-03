518ca393890b0c0789f0fcfa74f16559a46e5d71
thanghaqn2000 <57361234+thanghaqn2000@users.noreply.github.com>
2021-10-03 11:01:12 +0700

    update entity

--- InternetServiceManagement/src/main/java/com/example/demo/entity/Pay.java
+++ InternetServiceManagement/src/main/java/com/example/demo/entity/Pay.java
@@ -1,4 +1,53 @@
 package com.example.demo.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+@Entity
 public class Pay {
+    @Id
+    private int payId;
+    private double totalPayment;
+    private String status;
+
+    @ManyToOne(targetEntity = Customer.class)
+    @JoinColumn(name = "customerId", referencedColumnName = "customerId")
+    private Customer customer;
+
+    public Pay() {
+    }
+
+    public int getPayId() {
+        return payId;
+    }
+
+    public void setPayId(int payId) {
+        this.payId = payId;
+    }
+
+    public double getTotalPayment() {
+        return totalPayment;
+    }
+
+    public void setTotalPayment(double totalPayment) {
+        this.totalPayment = totalPayment;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
 }
