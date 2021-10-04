cbc42de5450f54abcd3f2f7a36ae0ce508f4d116
201000thanhnguyen <nhu1thangtrau@gmail.com>
2021-10-04 21:16:26 +0700

    ThanhNHM-ver6

--- InternetServiceManagement/src/main/java/com/example/demo/repository/ServiceRepository.java
+++ InternetServiceManagement/src/main/java/com/example/demo/repository/ServiceRepository.java
@@ -1,4 +1,7 @@
 package com.example.demo.repository;
 
-public interface ServiceRepository {
+import com.example.demo.entity.Service;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ServiceRepository extends JpaRepository<Service, String> {
 }
