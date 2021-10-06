31f4fdd8fdb399e9960ca4b9bb121b06a2e2054d
A2010I1-DoVietDangKhoa <dokhoa342@gmail.com>
2021-10-06 10:47:07 +0700

    customer create

--- InternetServiceManagement/src/main/java/com/example/demo/entity/Customer.java
+++ InternetServiceManagement/src/main/java/com/example/demo/entity/Customer.java
@@ -1,19 +1,29 @@
 package com.example.demo.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import javax.persistence.*;
 import java.util.Set;
 
 @Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 public class Customer {
     @Id
-    private int customerId;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer customerId;
     private String fullName;
     private String dateOfBirth;
     private String email;
     private String address;
     private String phone;
-    private boolean status;
-    private String idCard;
+    private Boolean status;
+//    private String idCard;
 
     @OneToOne(cascade = CascadeType.ALL)
     @JoinColumn(name = "userName",referencedColumnName = "userName")
@@ -28,102 +38,4 @@ public class Customer {
     @OneToMany(mappedBy = "customer", cascade = {CascadeType.ALL,CascadeType.REMOVE})
     Set<Order> orders;
 
-    public Customer() {
-    }
-
-    public int getCustomerId() {
-        return customerId;
-    }
-
-    public void setCustomerId(int customerId) {
-        this.customerId = customerId;
-    }
-
-    public String getFullName() {
-        return fullName;
-    }
-
-    public void setFullName(String fullName) {
-        this.fullName = fullName;
-    }
-
-    public String getDateOfBirth() {
-        return dateOfBirth;
-    }
-
-    public void setDateOfBirth(String dateOfBirth) {
-        this.dateOfBirth = dateOfBirth;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public String getPhone() {
-        return phone;
-    }
-
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
-
-    public boolean isStatus() {
-        return status;
-    }
-
-    public void setStatus(boolean status) {
-        this.status = status;
-    }
-
-    public String getIdCard() {
-        return idCard;
-    }
-
-    public void setIdCard(String idCard) {
-        this.idCard = idCard;
-    }
-
-    public Account getAccount() {
-        return account;
-    }
-
-    public void setAccount(Account account) {
-        this.account = account;
-    }
-
-    public Set<OrderService> getOrderServices() {
-        return orderServices;
-    }
-
-    public void setOrderServices(Set<OrderService> orderServices) {
-        this.orderServices = orderServices;
-    }
-
-    public Set<Pay> getPays() {
-        return pays;
-    }
-
-    public void setPays(Set<Pay> pays) {
-        this.pays = pays;
-    }
-
-    public Set<Order> getOrders() {
-        return orders;
-    }
-
-    public void setOrders(Set<Order> orders) {
-        this.orders = orders;
-    }
 }
--- InternetServiceManagement/src/main/java/com/example/demo/service/impl/CustomerServiceImpl.java
+++ InternetServiceManagement/src/main/java/com/example/demo/service/impl/CustomerServiceImpl.java
@@ -1,8 +1,49 @@
 package com.example.demo.service.impl;
 
+import com.example.demo.entity.Account;
+import com.example.demo.entity.Customer;
+import com.example.demo.http.request.CustomerRequest;
+import com.example.demo.repository.AccountRepository;
+import com.example.demo.repository.CustomerRepository;
 import com.example.demo.service.CustomerService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CustomerServiceImpl implements CustomerService {
+    private final CustomerRepository customerRepository;
+    private final AccountRepository accountRepository;
+
+    @Autowired
+    private CustomerServiceImpl(CustomerRepository customerRepository,
+                                AccountRepository accountRepository){
+        this.customerRepository = customerRepository;
+        this.accountRepository = accountRepository;
+    }
+
+
+    @Override
+    public void createCustomer(CustomerRequest customerRequest) {
+        if (customerRequest.getPassword().equals(customerRequest.getPasswordRetype())){
+            Customer customer = new Customer();
+            Account account = new Account();
+            customer.setFullName(customerRequest.getFullName());
+            customer.setAddress(customerRequest.getAddress());
+            customer.setDateOfBirth(customerRequest.getDateOfBirth());
+            customer.setEmail(customerRequest.getEmail());
+            customer.setPhone(customerRequest.getPhone());
+            customer.setStatus(customerRequest.getStatus());
+            account.setUserName(customerRequest.getUsername());
+            account.setPassword(customerRequest.getPassword());
+            customer.setAccount(account);
+            account.setCustomer(customer);
+            customerRepository.save(customer);
+            accountRepository.save(account);
+        }
+    }
+
+    @Override
+    public void updateCustomer(CustomerRequest customerRequest) {
+
+    }
 }
