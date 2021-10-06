aa24db3487068a311e99de18251157abe039fb05
Cuong <64198222+leesyaoran123@users.noreply.github.com>
2021-10-06 18:50:05 +0700

    6/10

--- InternetServiceManagement/src/main/java/com/example/demo/controller/CustomerController.java
+++ InternetServiceManagement/src/main/java/com/example/demo/controller/CustomerController.java
@@ -2,20 +2,34 @@ package com.example.demo.controller;
 
 import com.example.demo.entity.Customer;
 import com.example.demo.service.CustomerService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping(value = "/customer")
 public class CustomerController {
+    @Autowired
     private CustomerService customerService;
 
-    @GetMapping(value = {"/info/{id}"})
+    @PostMapping("/create")
+    public ResponseEntity<Customer> saveCustomer(@RequestBody Customer blog){
+        customerService.save(blog);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @GetMapping(value = {"/info/{id}"}, produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<Customer> customerInfo(@PathVariable String id, Model model){
         Customer customer = customerService.findById(id);
-        model.addAttribute("blogInfo", customer);
+        if (customer == null){
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+
         return new ResponseEntity<>(customer, HttpStatus.OK);
     }
 
