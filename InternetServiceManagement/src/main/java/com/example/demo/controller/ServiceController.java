9e8ea643a44caa7ec600cae64238439ce34c7ea6
MAIDOAN2302 <doanthimai23021992@gmail.com>
2021-10-18 23:29:05 +0700

    update 18/10/2021

--- InternetServiceManagement/src/main/java/com/example/demo/controller/ServiceController.java
+++ InternetServiceManagement/src/main/java/com/example/demo/controller/ServiceController.java
@@ -7,7 +7,6 @@ import org.springframework.http.HttpStatus;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.MediaType;
@@ -15,7 +14,6 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
 
 @RestController
 @RequestMapping(value = "/service")
@@ -41,7 +39,7 @@ public class ServiceController {
     }
 
     @GetMapping(value = "/list")
-    public ResponseEntity<Page<Service>> listAllService(@PageableDefault(size = 4) Pageable pageable) {
+    public ResponseEntity<Page<Service>> listAllService(@PageableDefault(size = 5) Pageable pageable) {
         Page<Service> serviceList = serviceService.findAllService(pageable);
         if (serviceList.isEmpty()) {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
