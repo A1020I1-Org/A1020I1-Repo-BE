a79ee63fce935ca2d44b0f3f4fb579c0699aa781
ThuanDV <15i1dangvanthuan@gmail.com>
2021-10-20 21:32:25 +0700

    update code BE

--- InternetServiceManagement/src/main/java/com/example/demo/controller/ComputerController.java
+++ InternetServiceManagement/src/main/java/com/example/demo/controller/ComputerController.java
@@ -7,9 +7,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.web.PageableDefault;
 
 import javax.validation.Valid;
 
@@ -32,51 +29,13 @@ public class ComputerController {
     }
 
     @RequestMapping("/update/{id}")
-    public ResponseEntity<Computer> update(@Valid @RequestBody Computer computer, @PathVariable String id) {
-        this.computerService.findById(id);
-        if (this.computerService.findById(id) == null) {
+    public ResponseEntity<Computer> update(@Valid @RequestBody Computer computer,  BindingResult bindingResult, @PathVariable int id) {
+        if (bindingResult.hasFieldErrors()) {
             return new ResponseEntity<>(HttpStatus.NOT_ACCEPTABLE);
         } else {
+            this.computerService.findById(id);
             this.computerService.save(computer);
-            return new ResponseEntity<>(HttpStatus.CREATED);
+            return new ResponseEntity<>(HttpStatus.OK);
         }
     }
-  
-   @GetMapping("/listComputer")
-    public ResponseEntity<Page<Computer>> listComputer(@PageableDefault(size = 4) Pageable pageable) {
-        Page<Computer> computerList = computerService.finAll(pageable);
-        if (computerList.isEmpty()) {
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        }
-        return new ResponseEntity<>(computerList, HttpStatus.OK);
-    }
-
-    @DeleteMapping("/deleteComputer/{id}")
-    public ResponseEntity<Computer> deleteComputer(@PathVariable String id) {
-        Computer computer = computerService.findById(id);
-        if (computer == null) {
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        }
-        computerService.delete(id);
-        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-    }
-
-    @GetMapping("/searchComputer")
-    public ResponseEntity<Page<Computer>> searchComputer(@RequestParam("computerId") String id,
-                                                         @RequestParam("computerLocation") String computerLocation,
-                                                         @RequestParam("computerStartUsedDate") String computerStartUsedDate,
-                                                         @RequestParam("computerStartUsedTo") String computerStartUsedTo,
-                                                         @RequestParam("type") String type,
-                                                         @RequestParam("status") String status,
-                                                         @PageableDefault(size = 4) Pageable pageable) {
-        Page<Computer> computers = computerService.search(id, computerLocation, computerStartUsedDate,computerStartUsedTo, type, status,pageable);
-        if (computers.isEmpty()){
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        }
-        return new ResponseEntity<>(computers, HttpStatus.OK);
-
-    }
-  
 }
-
-
--- InternetServiceManagement/src/main/java/com/example/demo/entity/Computer.java
+++ InternetServiceManagement/src/main/java/com/example/demo/entity/Computer.java
@@ -9,7 +9,9 @@ import javax.persistence.*;
 import java.util.Set;
 
 @Entity
+
 public class Computer {
+
     @Id
     @Pattern(regexp = "^CP[\\d]{4}$", message = "Mã máy tính phải đúng định dạng CPXXXX (X từ 0-9)")
     private String computerId;
@@ -27,11 +29,13 @@ public class Computer {
     Set<Order> orders;
 
     @NotBlank(message = "Hãng sản xuất không được để trống")
-    private String computerId;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int computerId;
     private String computerLocation;
     private String computerStartUsedDate;
     private String computerWarrantyPeriod;
     private String computerConfiguration;
+    private String computerIpLocal;
 
     @OneToMany(mappedBy = "computer", cascade = {CascadeType.ALL,CascadeType.REMOVE})
     Set<Order> orders;
@@ -76,11 +80,11 @@ public class Computer {
     public Computer() {
     }
   
-    public String getComputerId() {
+    public int getComputerId() {
         return computerId;
     }
 
-    public void setComputerId(String computerId) {
+    public void setComputerId(int computerId) {
         this.computerId = computerId;
     }
 
@@ -114,6 +118,14 @@ public class Computer {
 
     public void setComputerConfiguration(String computerConfiguration) {
         this.computerConfiguration = computerConfiguration;
+    }
+
+    public String getComputerIpLocal() {
+        return computerIpLocal;
+    }
+
+    public void setComputerIpLocal(String computerIpLocal) {
+        this.computerIpLocal = computerIpLocal;
     }
 
     public Set<Order> getOrders() {
